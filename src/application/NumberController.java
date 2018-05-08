0d453ad2b04e0a60a22aacfc4d52b63b9a1d11e2
Visurt Anuttivong <visurt.a@ku.th>
2018-05-08 13:48:22 +0700

    add dialog

--- src/application/NumberController.java
+++ src/application/NumberController.java
@@ -1,22 +1,20 @@
 package application;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class NumberController {
@@ -63,13 +61,13 @@ public class NumberController {
 		
 		secController.setArr(intArray);
 
-//		Stage stage = new Stage();
 		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//		Stage stage = new Stage();
 		stage.setScene(new Scene(root));
 		stage.show();
 	}
 
-	public void handleRandom(ActionEvent event) {
+	public void handleRandom(ActionEvent event) throws IOException {
 		defaultfield.setVisible(false);
 
 		boolean isSet = false;
@@ -107,7 +105,17 @@ public class NumberController {
 				isSet = true;
 				randomed.setText("?");
 				numbers = new ArrayList<Integer>();
-				System.out.println("You generated all numbers in between min and max. I gonna start over again");
+				final Stage dialog = new Stage();
+                dialog.initModality(Modality.APPLICATION_MODAL);
+                Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                dialog.initOwner(primaryStage);
+                
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("DialogUI.fxml"));
+        		Parent root = (Parent) loader.load();
+                
+                Scene dialogScene = new Scene(root);
+                dialog.setScene(dialogScene);
+                dialog.show();
 			}
 		}
 
--- src/application/RecentNumberController.java
+++ src/application/RecentNumberController.java
@@ -8,22 +8,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 public class RecentNumberController {
 	@FXML
-	private ListView list;
+	private ListView<Integer> list;
 
 	private boolean isReset = false;
-	
+
 	private int[] array;
 
 	public void setArr(int[] arr) {
 		array = arr;
-		for (int i : arr) {
-			list.getItems().add(i);
+		for (int i = arr.length - 1; i >= 0; i--) {
+			list.getItems().add(arr[i]);
 		}
 	}
 
@@ -31,13 +30,13 @@ public class RecentNumberController {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("NumberUI.fxml"));
 		Parent root = (Parent) loader.load();
 		NumberController secController = loader.getController();
-		
+
 		if (!isReset) {
 			secController.setList(array);
 		} else {
 			secController.setList(new int[0]);
 		}
-		
+
 		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		stage.setScene(new Scene(root));
 		stage.show();
